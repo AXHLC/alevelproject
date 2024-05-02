490e7fb215ac3974e31f2274b3b81d8187b36aad
Ahmed Chaal <144357083+AXHLC@users.noreply.github.com>
2024-05-02 10:51:58 +0100

    Create myvalidator.py
    added myvalidator

---
+++ myvalidator.py
@@ -0,0 +1,46 @@
+def lengthscheck(string, nums, option):
+
+    # option 1 is to check the nums of string is equal to the nums necessary
+    if option == 1:
+        if len(string) == nums:
+            return True
+        else:
+            return False
+    # option 2 is to check the nums of string is greater than or equal to the nums
+    elif option == 2:
+        if len(string) >= nums:
+            return True
+        else:
+            return False
+    # option 3 is to check the nums of string is less than or equal to the nums
+    elif option == 3:
+        if len(string) <= nums:
+            return True
+        else:
+            return False
+
+def emailcheck(email):
+    import re
+    # This variable is the format in which the email will be checked against
+    regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+    # if the email matches the format given then returns true otherwise returns false
+    if re.fullmatch(regex, email):
+        return True
+    else:
+        return False
+
+def passcheck(password):
+    import re
+    # this validates a string eneteres to include numbers, capital letters, and special characters
+    if re.search('\d', password) and re.search(r'[A-Z]', password) and re.search('[+,?,#,£,$,%,&]', password):
+        return True
+    else:
+        return False
+
+
+# testing
+if __name__ == "__main__":
+    #print(numscheck("b3338", 10, 3))
+    #print(emailcheck("dirguhdfklj"))
+    print(passcheck("t8hareeAnonums?"))
+
