8e9b3828a302832805dc4d2b249e012f0495a4cd
Ahmed Chaal <144357083+AXHLC@users.noreply.github.com>
2024-05-04 11:54:34 +0100

    added class for myvalidator.py in order to make instances of objects later, for example passwordcheck being the default blueprint for all the other objects. I have also simplified the passcheck function to make it look shorter at the if statement

--- myvalidator.py
+++ myvalidator.py
@@ -1,61 +1,42 @@
+import re
 
-def lengthscheck(string, nums, option):
+class MyValidators:
 
-    # option 1 is to check the nums of string is equal to the nums necessary
-    if option == 1:
-        if len(string) == nums:
-            return True
-        else:
+    def __init__(self):
+        pass
+
+    def lengthscheck(self, string, nums, option):
+        if option == 1:
+            if len(string) == nums:
+                return True
             return False
-    # option 2 is to check the nums of string is greater than or equal to the nums
-    elif option == 2:
-        if len(string) >= nums:
-            return True
-        else:
+        elif option == 2:
+            if len(string) >= nums:
+                return True
             return False
-    # option 3 is to check the nums of string is less than or equal to the nums
-    elif option == 3:
-        if len(string) <= nums:
-            return True
-        else:
+        elif option == 3:
+            if len(string) <= nums:
+                return True
             return False
 
-def emailcheck(email):
-    import re
-    # This variable is the format in which the email will be checked against
-    regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
-    # if the email matches the format given then returns true otherwise returns false
-    if re.fullmatch(regex, email):
-        return True
-    else:
-        return False
+    def emailcheck(self, email):
+        regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+        return bool(re.fullmatch(regex, email))
 
-def passcheck(password):
-    import re
-    # this validates a string entered to include numbers, capital letters, and special characters
-    if re.search('\d', password) and re.search(r'[A-Z]', password) and re.search('[+,?,#,£,$,%,&]', password):
-        return True
-    else:
-        return False
+    def passcheck(self, password):
+        return bool(re.search(r'(?=.*\d)(?=.*[A-Z])(?=.*[+,?,#,£,%,&])', password))
 
-def rangecheck(number, lower, upper):
-    if number >= lower and number <= upper:
-        return True
-    else:
-        return False
+    def rangecheck(self, number, lower, upper):
+        return lower <= number <= upper
 
-def prescheck(inp):
-    while inp == "":
-        return False
-    else:
-        return True
+    def prescheck(self, inp):
+        return bool(inp)
 
 # testing
 if __name__ == "__main__":
-    #print(lengthscheck("b3338", 10, 3))
-    #print(emailcheck("dirguhdfklj"))
-    #print(passcheck("t8hareeAnonums"))
-    print(rangecheck(23, 20, 60))
-    #print(prescheck("b"))
-
-
+    validator = MyValidators()
+    print(validator.lengthscheck("b3338", 10, 3))
+    print(validator.emailcheck("dirguh@dfklj.com"))
+    print(validator.passcheck("t8hareeAnonu?ms"))
+    print(validator.rangecheck(23, 2, 60))
+    print(validator.prescheck(""))
--- unittests.py
+++ unittests.py
@@ -1,19 +1,21 @@
 import unittest
-from myvalidator import lengthscheck  # the lengthscheck function is in the myvalidator.py file
+from myvalidator import MyValidators
 
 class TestLengthsCheck(unittest.TestCase):
+    def setUp(self):
+        self.validator = MyValidators()
 
     def test_equal_length(self):
-        self.assertTrue(lengthscheck("hello", 5, 1))
-        self.assertFalse(lengthscheck("hello", 6, 1))
+        self.assertTrue(self.validator.lengthscheck("hello", 5, 1))
+        self.assertFalse(self.validator.lengthscheck("hello", 6, 1))
 
     def test_greater_than_length(self):
-        self.assertTrue(lengthscheck("hello", 4, 2))
-        self.assertFalse(lengthscheck("hello", 6, 2))
+        self.assertTrue(self.validator.lengthscheck("hello", 4, 2))
+        self.assertFalse(self.validator.lengthscheck("hello", 6, 2))
 
     def test_less_than_length(self):
-        self.assertTrue(lengthscheck("hello", 6, 3))
-        self.assertFalse(lengthscheck("hello", 4, 3))
+        self.assertTrue(self.validator.lengthscheck("hello", 6, 3))
+        self.assertFalse(self.validator.lengthscheck("hello", 4, 3))
 
 if __name__ == '__main__':
     unittest.main()
