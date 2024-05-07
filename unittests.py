e2895e0fbd64f9c28fe5d49f89568cd4e3cfebab
Ahmed Chaal <144357083+AXHLC@users.noreply.github.com>
2024-05-07 10:32:13 +0100

    started my email check unit testing

--- unittests.py
+++ unittests.py
@@ -22,7 +22,28 @@ class TestLengthsCheck(unittest.TestCase):
         self.assertTrue(self.validator.lengthscheck("hello", 6, 3))
         self.assertFalse(self.validator.lengthscheck("hello", 4, 3))
 
+class TestEmailCheck(unittest.TestCase):
 
+    # setUp method is there to set up TheValidation object from myvalidator.py
+    def setUp(self):
+        self.validator = TheValidation()
+
+    # test_email_check method is there to test the emailcheck method from myvalidator.py
+    def test_email_check(self):
+        valid_emails = [
+            'user@example.com',
+            'email@example.co.jp',
+            'firstname-lastname@example.com',
+            'email@example.museum',
+            'email@example.name',
+            '_______@example.com',
+            '1234567890@example.com',
+            'email@123.123.123.123'
+        ]
+
+        for email in valid_emails:
+            # Tests valid emails
+            self.assertTrue(self.validator.emailcheck(email))
 # testing
 if __name__ == '__main__':
     unittest.main()
