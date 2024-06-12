c3acf9527c636eb5b1547620feb2346f2b97c830
Ahmed Chaal <144357083+AXHLC@users.noreply.github.com>
2024-06-12 17:57:01 +0100

    Made my coach menu added accounts and profile tabs with the sub tabs

--- demo.py
+++ demo.py
@@ -1,30 +1,71 @@
 import tkinter as tk
 from tkinter import Menu
-
-class tabs:
-    def __init__(self):
+# pandas for chart designs
+class Menus:
+    def __init__(self, aors):
         # root window has been made a class variable
         self.win = tk.Tk()
         # creates a window and made it a class variable
-        self.win.title('title')
-        # set window title to 'title'
+        self.win.title(aors)
+        # set window title to what is passed in the class whether admin menu or student menu
         self.menubar = Menu(self.win)
         # this creates the bar for the menu
         self.win.config(menu=self.menubar)
 
-        self.accounts_menu = Menu(self.menubar, tearoff=False)
-        self.accounts_menu.add_command(label='Settings', command=lambda: self.settings())
-        self.menubar.add_cascade(label="Accounts", menu=self.accounts_menu)
-# pandas for chart design
+        # Create 'Settings' menu
+        self.settings_tab = Menu(self.menubar, tearoff=False)
+        self.settings_tab.add_command(label='colours', command=lambda: self.colours())
+        self.settings_tab.add_command(label='Fonts', command=lambda: self.fonts())
+        self.menubar.add_cascade(label="Settings", menu=self.settings_tab)
+
+    def coachmenu(self):
+        # creates Accounts tab
+        self.accounts_tab = Menu(self.menubar, tearoff=False)
+        self.accounts_tab.add_command(label='New', command=lambda: self.new())
+        self.accounts_tab.add_command(label='Update', command=lambda: self.update())
+        self.accounts_tab.add_command(label='Remove', command=lambda: self.remove())
+        self.accounts_tab.add_command(label='Change Password', command=lambda: self.changepass())
+        self.menubar.add_cascade(label="Accounts", menu=self.accounts_tab)
+
+        # creates profile management tab
+        self.profile_tab = Menu(self.menubar, tearoff=False)
+        self.profile_tab.add_command(label='View Profile', command=lambda: self.viewprofile())
+        self.profile_tab.add_command(label='Enter Performance', command=lambda: self.enterperf())
+        self.profile_tab.add_command(label='Set Targets', command=lambda: self.settargets())
+        self.menubar.add_cascade(label="Profiles", menu=self.profile_tab)
 
         self.win.mainloop()
 
-    def settings(self):
-        s = "Settings"
+
+    def colours(self):
+        s = "Colours"
+        print("You have clicked " + s)
+    def fonts(self):
+        s = "Fonts"
+        print("You have clicked " + s)
+    def new(self):
+        s = "New"
+        print("You have clicked " + s)
+    def update(self):
+        s = "Update"
+        print("You have clicked " + s)
+    def remove(self):
+        s = "Remove"
+        print("You have clicked " + s)
+    def changepass(self):
+        s = "Change Password"
+        print("You have clicked " + s)
+    def viewprofile(self):
+        s = "View Profile"
+        print("You have clicked " + s)
+    def enterperf(self):
+        s = "Enter Performance"
         print("You have clicked " + s)
 
-    def file(self):
-        s = "File"
+    def settargets(self):
+        s = "Set Targets"
         print("You have clicked " + s)
 
-tabs()
+
+
+Menus('Coach').coachmenu()
