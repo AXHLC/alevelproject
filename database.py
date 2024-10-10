8a779135242714ca9eab7684865d5ce8addd394b
Ahmed Chaal <144357083+AXHLC@users.noreply.github.com>
2024-10-10 15:29:31 +0100

    just finished doing my functions for data managment in my databse and tested them

--- database.py
+++ database.py
@@ -65,8 +65,63 @@ class Database:
     def close(self):
         if self.conn:
             self.conn.close()
+    
+    def InsertData(self, username, first_name, last_name, password, role):
+        conn = sqlite3.connect('basketball_tracker.db')
+        # insert data into database table
+        conn.execute('''INSERT INTO users  (username, first_name, last_name, password, role) values (?, ?, ?, ?, ?)''',
+            (username, first_name, last_name, password, role))
+        conn.commit()  
+        conn.close()
+    
+    def UpdateUsername(self, userid, newusername):
+        conn = sqlite3.connect('basketball_tracker.db')
+        conn.execute('''UPDATE users SET username = ? WHERE user_id = ?''', (newusername, userid))
+        print(f"Updated username to {newusername} for user_id {userid}")
+        conn.commit()
+        conn.close()
+
+    def UpdateFirstName(self, userid, newfn):
+        conn = sqlite3.connect('basketball_tracker.db')
+        conn.execute('''UPDATE users SET username = ? WHERE user_id = ?''', (newfn, userid))
+        print(f"Updated username to {newfn} for user_id {userid}")
+        conn.commit()
+        conn.close()
+
+    def UpdateLastName(self, userid, newln):
+        conn = sqlite3.connect('basketball_tracker.db')
+        conn.execute('''UPDATE users SET username = ? WHERE user_id = ?''', (newln, userid))
+        print(f"Updated username to {newln} for user_id {userid}")
+        conn.commit()
+        conn.close()
+    
+    def UpdatePassword(self, userid, newpass):
+        conn = sqlite3.connect('basketball_tracker.db')
+        conn.execute('''UPDATE users SET username = ? WHERE user_id = ?''', (newpass, userid))
+        print(f"Updated username to {newpass} for user_id {userid}")
+        conn.commit()
+        conn.close()
+    
+    def UpdateRole(self, userid, newrole):
+        conn = sqlite3.connect('basketball_tracker.db')
+        conn.execute('''UPDATE users SET username = ? WHERE user_id = ?''', (newrole, userid))
+        print(f"Updated username to {newrole} for user_id {userid}")
+        conn.commit()
+        conn.close() 
+
+    def DeleteRecord(self, userid):
+        conn = sqlite3.connect('basketball_tracker.db')
+        conn.execute("DELETE FROM users WHERE  user_id=?",(userid ,) )
+    
+        conn.commit()
+        conn.close()
 
 # Example usage
 if __name__ == "__main__":
     db = Database('basketball_tracker.db')
+    #db.InsertData('jodoe', 'J', 'D', 'password', 'player')
+    #db.InsertData('manjack', 'MAN', 'JACK', 'pass', 'player')
+    #db.InsertData('juniroyal', 'Junior', 'royal', 'word', 'player')
+    db.UpdateUsername(6, 'juniroyal')
+    #db.DeleteRecord(5)
     db.close()
