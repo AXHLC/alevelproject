cd0fdf27120994e5fb6fb3dbc7bef3858d0583e2
Ahmed Chaal <144357083+AXHLC@users.noreply.github.com>
2024-11-14 13:53:00 +0000

    made things fir in screen

--- adminplayerwindows.py
+++ adminplayerwindows.py
@@ -15,7 +15,7 @@ class BaseWindow:
             raise ValueError("Invalid role. Enter 'coach' for admin menu or 'player' for student menu.")
 
         # Set the default window size with golden ratio 1.6
-        self.win.geometry("1120x700")
+        self.win.geometry("640x400")
 
         # Make the window unresizable
         self.win.resizable(False, False)
